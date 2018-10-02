43ee62930eda4131d7f2b6f3c5b040489206f98b
zyhome <a>
2018-10-02 17:46:11 +0800

    e3

---
+++ spring101-webmvc/src/main/java/me/josephzhu/spring101webmvc/Spring101WebmvcApplication.java
@@ -0,0 +1,12 @@
+package me.josephzhu.spring101webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring101WebmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring101WebmvcApplication.class, args);
+    }
+}
